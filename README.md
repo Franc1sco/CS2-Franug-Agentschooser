2c656fe222ec53b92ec4ae0d0b7fd6ca2670a146
Franc1sco Franug <franug13@gmail.com>
2024-02-06 20:01:50 +0100

    Update

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
-### Por Hacer
+### Agents Chooser
 
-Probar las rutas de los agentes con el css_setmodel de https://github.com/cola-team-org/Cola-css-basic-admin y ver cual es cual de aquí https://wiki.cs.money/agents para ponerles el nombre a cada agente aparte de la ruta que se saca del archivo pak01_dir.vpk adjunto.
+Plugin for choose agents with !agents
 
-Ejemplo: css_setmodel @all characters\models\tm_professional\tm_professional_varf5.vmdl
+### Requirements
 
-<img width="290" alt="image" src="https://github.com/Franc1sco/CS2-Franug-Agentschooser/assets/5090660/35424dc7-bffc-44df-ad51-e4f31b8e8895">
+* [CounterStrikeSharp](https://github.com/roflmuffin/CounterStrikeSharp/) (version 154 or higher)
+
+### Installation
+
+Drag and drop from the BUILD section to game/csgo/addons/counterstrikesharp/plugins
+
+
+### Configuration
+
+Configure the file FranugAgentsChooser.json generated on addons/counterstrikesharp/configs/plugins/FranugAgentsChooser
--- src/FranugAgentsChooser.cs
+++ src/FranugAgentsChooser.cs
@@ -1,21 +1,119 @@
 ﻿using CounterStrikeSharp.API;
 using CounterStrikeSharp.API.Core;
 using CounterStrikeSharp.API.Core.Attributes;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Menu;
+using CounterStrikeSharp.API.Modules.Utils;
+using CounterStrikeSharp.API.Modules.Admin;
+using System.Text.Json;
 using Microsoft.Data.Sqlite;
+using System.Text.Json.Serialization;
 
 namespace FranugAgentsChooser;
 
+public class ConfigGen : BasePluginConfig
+{
+    [JsonPropertyName("AccessFlag")] public string AccessFlag { get; set; } = "";
+    [JsonPropertyName("CtAgents")] public int CtAgents { get; set; } = 3;
+    [JsonPropertyName("TtAgents")] public int TtAgents { get; set; } = 3;
+}
 
-[MinimumApiVersion(142)]
-public class FranugAgentsChooser : BasePlugin
+
+[MinimumApiVersion(154)]
+public class FranugAgentsChooser : BasePlugin, IPluginConfig<ConfigGen>
 {
     public override string ModuleName => "Franug Agents Chooser";
     public override string ModuleAuthor => "Franc1sco Franug";
-    public override string ModuleVersion => "0.0.1";
+    public override string ModuleVersion => "0.0.3";
+    public ConfigGen Config { get; set; } = null!;
+    public void OnConfigParsed(ConfigGen config) { Config = config; }
+
+    private string TAG = $" {ChatColors.Lime}[AgentsChooser]{ChatColors.Green} ";
 
     private SqliteConnection? connectionSQLITE = null;
     internal static Dictionary<int, AgentsInfo> gAgentsInfo = new Dictionary<int, AgentsInfo>();
 
+    internal static readonly Dictionary<string, string> agentsListCT = new()
+    {
+        {"characters\\models\\ctm_fbi\\ctm_fbi_varianta.vmdl", "CounterTerrorist | FBI"},
+        {"characters\\models\\ctm_fbi\\ctm_fbi_variantd.vmdl", "CounterTerrorist v2 | FBI"},
+        {"characters\\models\\ctm_fbi\\ctm_fbi_variantb.vmdl", "Special Agent Ava | FBI"},
+        {"characters\\models\\ctm_fbi\\ctm_fbi_variantc.vmdl", "Markus Delrow | FBI HRT"},
+		{"characters\\models\\ctm_fbi\\ctm_fbi_variantf.vmdl", "Markus Delrow v2| FBI HRT"},
+        {"characters\\models\\ctm_fbi\\ctm_fbi_variantg.vmdl", "Operator | FBI SWAT"},
+        {"characters\\models\\ctm_diver\\ctm_diver_varianta.vmdl", "Cmdr. Davida 'Goggles' Fernandez | SEAL Frogman"},
+        {"characters\\models\\ctm_diver\\ctm_diver_variantb.vmdl", "Cmdr. Frank 'Wet Sox' Baroud | SEAL Frogman"},
+        {"characters\\models\\ctm_diver\\ctm_diver_variantc.vmdl", "Lieutenant Rex Krikey | SEAL Frogman"},
+        {"characters\\models\\ctm_gendarmerie\\ctm_gendarmerie_variantd.vmdl", "Chem-Haz Capitaine | Gendarmerie Nationale"},
+        {"characters\\models\\ctm_gendarmerie\\ctm_gendarmerie_variante.vmdl", "Officer Jacques Beltram | Gendarmerie Nationale"},
+        {"characters\\models\\ctm_gendarmerie\\ctm_gendarmerie_variantc.vmdl", "Chef d'Escadron Rouchard | Gendarmerie Nationale"},
+        {"characters\\models\\ctm_gendarmerie\\ctm_gendarmerie_variantb.vmdl", "Chem-Haz Capitaine | Gendarmerie Nationale"},
+        {"characters\\models\\ctm_gendarmerie\\ctm_gendarmerie_varianta.vmdl", "Sous-Lieutenant Medic | Gendarmerie Nationale"},
+        {"characters\\models\\ctm_st6\\ctm_st6_variantg.vmdl", "'Blueberries' Buckshot | NSWC SEAL"},
+		{"characters\\models\\ctm_st6\\ctm_st6_variantj.vmdl", "'Blueberries' Buckshot v2| NSWC SEAL"},
+        {"characters\\models\\ctm_st6\\ctm_st6_varianti.vmdl", "'Lt. Commander Ricksaw | NSWC SEAL"},
+        {"characters\\models\\ctm_st6\\ctm_st6_variantl.vmdl", "''Two Times' McCoy | USAF TACP"},
+        {"characters\\models\\ctm_st6\\ctm_st6_variantm.vmdl", "''Two Times' McCoy v2| USAF TACP"},
+        {"characters\\models\\ctm_st6\\ctm_st6_variantn.vmdl", "'Primeiro Tenente | Brazilian 1st Battalion"},
+        {"characters\\models\\ctm_swat\\ctm_swat_variante.vmdl", "'Cmdr. Mae 'Dead Cold' Jamison | SWAT"},
+        {"characters\\models\\ctm_swat\\ctm_swat_variantk.vmdl", "'Cmdr. Mae 'Dead Cold' Jamison v2 | SWAT"},
+        {"characters\\models\\ctm_swat\\ctm_swat_variantf.vmdl", "'1st Lieutenant Farlow | SWAT"},
+        {"characters\\models\\ctm_swat\\ctm_swat_variantg.vmdl", "John 'Van Healen' Kask | SWAT"},
+        {"characters\\models\\ctm_swat\\ctm_swat_varianth.vmdl", "Bio-Haz Specialist | SWAT"},
+        {"characters\\models\\ctm_swat\\ctm_swat_varianti.vmdl", "Sergeant Bombson | SWAT"},
+        {"characters\\models\\ctm_swat\\ctm_swat_variantj.vmdl", "Chem-Haz Specialist | SWAT"}
+    };
+
+    internal static readonly Dictionary<string, string> agentsListTT = new()
+    {
+        {"characters\\models\\tm_balkan\\tm_balkan_variantg.vmdl", "Rezan the Redshirt | Sabre"},
+        {"characters\\models\\tm_balkan\\tm_balkan_varianth.vmdl", "Rezan the Redshirt v2 | Sabre"},
+        {"characters\\models\\tm_balkan\\tm_balkan_variantf.vmdl", "Dragomir | Sabre Footsoldier"},
+        {"characters\\models\\tm_balkan\\tm_balkan_variantk.vmdl", "'The Doctor' Romanov | Sabre"},
+        {"characters\\models\\tm_balkan\\tm_balkan_varianti.vmdl", "Maximus | Sabre"},
+        {"characters\\models\\tm_balkan\\tm_balkan_variantj.vmdl", "Blackwolf | Sabre"},
+        {"characters\\models\\tm_balkan\\tm_balkan_variantl.vmdl", "Dragomir v2 | Sabre Footsoldier"},
+        {"characters\\models\\tm_jumpsuit\\tm_jumpsuit_varianta.vmdl", "Black | Yellow jacket"},
+        {"characters\\models\\tm_jumpsuit\\tm_jumpsuit_variantb.vmdl", "Beard | Yellow jacket"},
+        {"characters\\models\\tm_jumpsuit\\tm_jumpsuit_variantc.vmdl", "Bald | Yellow jacket"},
+        {"characters\\models\\tm_jungle_raider\\tm_jungle_raider_variantb.vmdl", "'Medium Rare' Crasswater | Guerrilla Warfare"},
+        {"characters\\models\\tm_jungle_raider\\tm_jungle_raider_variantb2.vmdl", "'Medium Rare' Crasswater v2 | Guerrilla Warfare"},
+        {"characters\\models\\tm_jungle_raider\\tm_jungle_raider_variantc.vmdl", "Arno The Overgrown | Guerrilla Warfare"},
+        {"characters\\models\\tm_jungle_raider\\tm_jungle_raider_variante.vmdl", "Vypa Sista of the Revolution | Guerrilla Warfare"},
+        {"characters\\models\\tm_jungle_raider\\tm_jungle_raider_variantd.vmdl", "Col. Mangos Dabisi | Guerrilla Warfare"},
+        {"characters\\models\\tm_jungle_raider\\tm_jungle_raider_variantf.vmdl", "Trapper Aggressor | Guerrilla Warfare"},
+        {"characters\\models\\tm_jungle_raider\\tm_jungle_raider_variantf2.vmdl", "Trapper Aggressor v2 | Guerrilla Warfare"},
+        {"characters\\models\\tm_leet\\tm_leet_varianta.vmdl", "Osiris | Elite Crew"},
+        {"characters\\models\\tm_leet\\tm_leet_variantb.vmdl", "Jungle Rebel | Elite Crew"},
+        {"characters\\models\\tm_leet\\tm_leet_variantj.vmdl", "Jungle Rebel v2 | Elite Crew"},
+        {"characters\\models\\tm_leet\\tm_leet_variantc.vmdl", "Prof. Shahmat | Elite Crew"},
+        {"characters\\models\\tm_leet\\tm_leet_varianti.vmdl", "Prof. Shahmat v2 | Elite Crew"},
+        {"characters\\models\\tm_leet\\tm_leet_variantd.vmdl", "Ground Rebel | Elite Crew"},
+        {"characters\\models\\tm_leet\\tm_leet_variante.vmdl", "Ground Rebel v2 | Elite Crew"},
+        {"characters\\models\\tm_leet\\tm_leet_variantg.vmdl", "Ground Rebel v3 | Elite Crew"},
+        {"characters\\models\\tm_leet\\tm_leet_varianth.vmdl", "Osiris | Elite Crew"},
+        {"characters\\models\\tm_leet\\tm_leet_variantf.vmdl", "The Elite Mr. Muhlik | Elite Crew"},
+        {"characters\\models\\tm_phoenix\\tm_phoenix_varianta.vmdl", "Terrorist | Phoenix"},
+        {"characters\\models\\tm_phoenix\\tm_phoenix_variantb.vmdl", "Terrorist v2 | Phoenix"},
+        {"characters\\models\\tm_phoenix\\tm_phoenix_variantc.vmdl", "Terrorist v3 | Phoenix"},
+        {"characters\\models\\tm_phoenix\\tm_phoenix_variantd.vmdl", "Terrorist v4 | Phoenix"},
+		{"characters\\models\\tm_phoenix\\tm_phoenix_varianti.vmdl", "Terrorist v5 | Phoenix"},
+        {"characters\\models\\tm_phoenix\\tm_phoenix_variantf.vmdl", "Enforcer | Phoenix"},
+        {"characters\\models\\tm_phoenix\\tm_phoenix_varianth.vmdl", "Soldier | Phoenix"},
+        {"characters\\models\\tm_phoenix\\tm_phoenix_variantg.vmdl", "Slingshot | Phoenix"},
+        {"characters\\models\\tm_phoenix_heavy\\tm_phoenix_heavy.vmdl", "Heavy Soldier | Phoenix"},
+        {"characters\\models\\tm_professional\\tm_professional_varf.vmdl", "Sir Bloody Miami Darryl | The Professionals"},
+        {"characters\\models\\tm_professional\\tm_professional_varf1.vmdl", "Sir Bloody Silent Darryl | The Professionals"},
+        {"characters\\models\\tm_professional\\tm_professional_varf2.vmdl", "Sir Bloody Skullhead Darryl | The Professionals"},
+        {"characters\\models\\tm_professional\\tm_professional_varf3.vmdl", "Sir Bloody Darryl Royale | The Professionals"},
+        {"characters\\models\\tm_professional\\tm_professional_varf4.vmdl", "Sir Bloody Loudmouth Darryl | The Professionals"},
+        {"characters\\models\\tm_professional\\tm_professional_varf5.vmdl", "Bloody Darryl The Strapped | The Professionals"},
+        {"characters\\models\\tm_professional\\tm_professional_varg.vmdl", "Safecracker Voltzmann | The Professionals"},
+        {"characters\\models\\tm_professional\\tm_professional_varh.vmdl", "Little Kev | The Professionals"},
+        {"characters\\models\\tm_professional\\tm_professional_vari.vmdl", "Number K | The Professionals"},
+        {"characters\\models\\tm_professional\\tm_professional_varj.vmdl", "Getaway Sally | The Professionals"}
+    };
 
     public override void Load(bool hotReload)
     {
@@ -62,6 +160,137 @@ public class FranugAgentsChooser : BasePlugin
                 return HookResult.Continue;
             }
         });
+
+        RegisterEventHandler<EventPlayerSpawn>(eventPlayerSpawn);
+    }
+
+
+    [ConsoleCommand("css_agents", "Select Agents model.")]
+    [CommandHelper(minArgs: 0, usage: "", whoCanExecute: CommandUsage.CLIENT_ONLY)]
+    public void setupMainMenu(CCSPlayerController? player, CommandInfo info)
+    {
+        if (!IsValidClient(player, false))
+        {
+            return;
+        }
+
+        if (Config.AccessFlag != "" && !AdminManager.PlayerHasPermissions(player, Config.AccessFlag))
+        {
+            player.PrintToChat(TAG + $"You dont have access to this command.");
+            return;
+        }
+
+        var menu = new CenterHtmlMenu("Agents Menu");
+        if ((Config.CtAgents == 1 && player.TeamNum == ((int)CsTeam.CounterTerrorist)) ||
+            (Config.CtAgents == 2 && player.TeamNum == ((int)CsTeam.Terrorist)) || 
+                Config.CtAgents == 3)
+        {
+            menu.AddMenuOption("CT Agents", (player, option) => {
+                setupCTMenu(player);
+            });
+        }
+
+        if ((Config.TtAgents == 1 && player.TeamNum == ((int)CsTeam.CounterTerrorist)) ||
+            (Config.TtAgents == 2 && player.TeamNum == ((int)CsTeam.Terrorist)) ||
+                Config.TtAgents == 3)
+        {
+            menu.AddMenuOption("TT Agents", (player, option) => {
+                setupTTMenu(player);
+            });
+        }
+
+        MenuManager.OpenCenterHtmlMenu(this, player, menu);
+    }
+
+    private void setupCTMenu(CCSPlayerController player)
+    {
+        var menu = new CenterHtmlMenu("Agents Menu - CT");
+        menu.AddMenuOption("No agent model", (player, option) => {
+
+            gAgentsInfo[(int)player.Index].AgentCT = "none";
+            player.PrintToChat(TAG + $"Set agent model to {ChatColors.Lime}" + "none" + $" {ChatColors.Green}on your next spawn");
+            updatePlayer(player);
+        });
+        foreach (var agent in agentsListCT)
+        {
+            menu.AddMenuOption(agent.Value, (player, option) => {
+
+                gAgentsInfo[(int)player.Index].AgentCT = agent.Key;
+                player.PrintToChat(TAG + $"Set agent model to {ChatColors.Lime}" + agent.Value);
+                if (IsValidClient(player) && (CsTeam)player.TeamNum == CsTeam.CounterTerrorist)
+                {
+                    player.PlayerPawn.Value.SetModel(gAgentsInfo[(int)player.Index].AgentCT);
+                    playerRefresh(player);
+                }
+                updatePlayer(player);
+            });
+        }
+        MenuManager.OpenCenterHtmlMenu(this, player, menu);
+    }
+
+    private void setupTTMenu(CCSPlayerController player)
+    {
+        var menu = new CenterHtmlMenu("Agents Menu - TT");
+        menu.AddMenuOption("No agent model", (player, option) => {
+
+            gAgentsInfo[(int)player.Index].AgentTT = "none";
+            player.PrintToChat(TAG + $"Set agent model to {ChatColors.Lime}" + "none" + $" {ChatColors.Green}on your next spawn");
+            updatePlayer(player);
+        });
+        foreach (var agent in agentsListTT)
+        {
+            menu.AddMenuOption(agent.Value, (player, option) => {
+
+                gAgentsInfo[(int)player.Index].AgentTT = agent.Key;
+                player.PrintToChat(TAG + $"Set agent model to {ChatColors.Lime}" + agent.Value);
+                if (IsValidClient(player) && (CsTeam)player.TeamNum == CsTeam.Terrorist)
+                {
+                    player.PlayerPawn.Value.SetModel(gAgentsInfo[(int)player.Index].AgentTT);
+                    playerRefresh(player);
+                }
+                updatePlayer(player);
+            });
+        }
+        MenuManager.OpenCenterHtmlMenu(this, player, menu);
+    }
+
+    private HookResult eventPlayerSpawn(EventPlayerSpawn @event, GameEventInfo info)
+    {
+        var player = @event.Userid;
+
+        if (!IsValidClient(player))
+        {
+            return HookResult.Continue;
+
+        }
+
+        AddTimer(2.5f, () =>
+        {
+            if (!IsValidClient(player))
+            {
+                return;
+            }
+
+            if ((CsTeam)player.TeamNum == CsTeam.CounterTerrorist) {
+
+                var agent_ct = gAgentsInfo[(int)player.Index].AgentCT;
+                if (agent_ct != null && !agent_ct.Equals("none"))
+                {
+                    player.PlayerPawn.Value.SetModel(agent_ct);
+                }
+            }
+            else if ((CsTeam)player.TeamNum == CsTeam.Terrorist)
+            {
+
+                var agent_tt = gAgentsInfo[(int)player.Index].AgentTT;
+                if (agent_tt != null && !agent_tt.Equals("none"))
+                {
+                    player.PlayerPawn.Value.SetModel(agent_tt);
+                }
+            }
+        });
+        
+        return HookResult.Continue;
     }
 
     private void updatePlayer(CCSPlayerController player)
@@ -242,6 +471,27 @@ public class FranugAgentsChooser : BasePlugin
         {
             await connectionSQLITE?.CloseAsync();
         }
+    }
+
+    private void playerRefresh(CCSPlayerController player)
+    {
+        string giveBack = "weapon_knife";
+        foreach (var weapon in player.PlayerPawn.Value.WeaponServices?.MyWeapons)
+        {
+            if (weapon is { IsValid: true, Value.IsValid: true } && (weapon.Value.DesignerName.Contains("knife") ||
+                weapon.Value.DesignerName.Contains("bayonet")))
+            {
+                giveBack = weapon.Value.DesignerName;
+                player.PlayerPawn.Value.RemovePlayerItem(weapon.Value);
+                break;
+            }
+        }
+        player.GiveNamedItem(giveBack);
+    }
+
+    private bool IsValidClient(CCSPlayerController client, bool isAlive = true)
+    {
+        return client != null && client.IsValid && client.PlayerPawn != null && client.PlayerPawn.IsValid && (!isAlive || client.PawnIsAlive) && !client.IsBot;
     }
 }
 
